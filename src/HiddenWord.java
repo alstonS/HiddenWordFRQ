3ff059d96931ccab5f347d632e3d999245bd5adb
AlstonS <Ashi3454@bths.edu>
2018-12-16 21:51:21 -0500

    Runner Complete

--- src/HiddenWord.java
+++ src/HiddenWord.java
@@ -1,28 +1,24 @@
 public class HiddenWord {
     private String word;
-    public HiddenWord(String word)
-    {
-        this.word=word;
+
+    public HiddenWord(String word) {
+        this.word = word;
     }
-    public getHint(String Puzzle)
-    {
-        for(int i=0; i < word.length();i++)
-        {
-            String hint = "";
-            if(Puzzle.substring(i,i+1).equals(word.substring(i,i+1)))
-            {
-                hint += Puzzle.substring(i,i+1);
-            }
-            else if(word.indexOf(Puzzle.substring(i,i+1))>=0)
-            {
+
+    public String getHint(String Puzzle) {
+        String hint = "";
+        for (int i = 0; i < word.length(); i++) {
+
+            if (Puzzle.substring(i, i + 1).equals(word.substring(i, i + 1))) {
+                hint += Puzzle.substring(i, i + 1);
+            } else if (word.indexOf(Puzzle.substring(i, i + 1)) >= 0) {
                 hint += "+";
+            } else {
+                hint += "*";
             }
-            else {
-            hint += "*";
-            }
-            return hint;
+
         }
 
+        return hint;
     }
-
 }
--- src/Runner.java
+++ src/Runner.java
@@ -4,9 +4,18 @@ public class Runner {
     public static void main(String[] args)
     {
         HiddenWord trial1 = new HiddenWord("APPLE");
-        String hint = "";
         Scanner in = new Scanner(System.in);
-
+        System.out.println("What's your guess?");
+        String guess = in.nextLine().substring(0,5).toUpperCase();
+        String hint = trial1.getHint(guess);
+        while(!hint.equals("APPLE")){
+            System.out.println("Your hint: " + hint);
+            System.out.println("What's your next guess?");
+            guess = in.nextLine().substring(0,5).toUpperCase();
+            hint = trial1.getHint(guess);
+        }
+        System.out.println("Congrats!");
+    }
 
     }
-}
+
